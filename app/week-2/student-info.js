9979baf2e5f564be2ef5b1ed42225ee8c43c2636
Bhavya2004-design <Bhavya1@edu.sait.ca>
2025-09-12 17:07:07 -0700

    week-2 Assignment - 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+export default function StudentInfo() {
+ return (
+ <main>
+ <h1>Bhavya</h1>
+ <Link href="/https://github.com/Bhavya2004-design">https://github.com/Bhavya2004-design</Link>
+ </main>
+ );
+}
