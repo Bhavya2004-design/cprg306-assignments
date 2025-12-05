71348913c04b4535a68e6fa99c2c10346c10ed9a
Bhavya2004-design <Bhavya1@edu.sait.ca>
2025-12-05 15:45:16 -0800

    week-10

--- app/week-9/_utils/firebase.js
+++ app/week-9/_utils/firebase.js
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6t9JUtoH1Tk1WJRnr_eOt7uhBuoZ0d8w",
-  authDomain: "cprg306-assignments-2c9df.firebaseapp.com",
-  projectId: "cprg306-assignments-2c9df",
-  storageBucket: "cprg306-assignments-2c9df.firebasestorage.app",
-  messagingSenderId: "225223590962",
-  appId: "1:225223590962:web:f59175e59da0d9b124282b",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
